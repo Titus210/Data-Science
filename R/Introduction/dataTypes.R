e5ce67a732867cc84ecc7f84674ce38dc6cd5036
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-27 23:02:35 +0300

    Data types in R

--- R/Introduction/dataTypes.R
+++ R/Introduction/dataTypes.R
@@ -1,3 +1,16 @@
 # locical datatype
 var_logical <- TRUE
-var_logical
+cat("The data type of variable logic is ", class(var_logical), "\n")
+
+var_numeric <- 2121
+cat("The data type of variable numeric is ", class(var_numeric), "\n")
+
+var_integer <- 1322L
+cat("The data type of variable integer is ", class(var_integer), "\n")
+
+var_complex <- 3 + 2i
+cat("The data type of variable complex is ", class(var_complex), "\n")
+
+var_character <- "Titus Kiplagat"
+cat("The data type of variable character is ", class(var_character), "\n")
+
